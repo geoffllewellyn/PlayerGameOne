e1adafaa4ca397f15b6ca8d099b68ba9d87fd557
geoffllewellyn <geoffllewellyn@gmail.com>
2020-08-15 15:20:08 -0700

    navmesh building

--- Assets/Scripts/Boundary.cs
+++ Assets/Scripts/Boundary.cs
@@ -2,75 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
+
 public class Boundary : MonoBehaviour 
 {
     public int boundarySize = 1;
-
-    GameObject[] boundary;
-    GameObject ground;
+    GameObject map;
     
     // Start is called before the first frame update
     void Start() 
     {
-        // GameObjects set up. boundary is the invisible box colliders in "WALLS"
-        boundary = GameObject.FindGameObjectsWithTag ("Boundry");
-        ground = GameObject.FindGameObjectsWithTag ("Ground")[0];
-        
-        // set up scales for angles parts.
-        float newScale = (float)boundarySize * 10;
-        float offsetScale = 2;
+        map = GameObject.FindGameObjectWithTag ("Map");
+        float newX = map.transform.localScale.x * boundarySize;
+        float newZ = map.transform.localScale.z * boundarySize;
+        map.transform.localScale = new Vector3 (newX, 1, newZ);
 
-        // loop through the walls.
-        // North, South, East and West
-        for (int i = 0; i < 4; i++) 
-        {
-            float offsetX = 0;
-            float offsetZ = 0;
-            if (i % 2 == 0) // first and third objects
-            {
-                boundary[i].transform.localScale = new Vector3(newScale, 10, 1);
-                if (boundary[i].name == "North") 
-                {
-                    offsetZ = -(boundarySize - offsetScale) * 0;
-                }
-                else if (boundary[i].name == "South") 
-                {
-                    offsetZ = (boundarySize - offsetScale) * 0;
-                }
-            }
-            else 
-            {
-                boundary[i].transform.localScale = new Vector3(1, 10, newScale);
-                if (boundary[i].name == "East") 
-                {
-                    offsetX = -(boundarySize - offsetScale);
-                }
-                else if (boundary[i].name == "West") 
-                {
-                    offsetX = (boundarySize - offsetScale);
-                }
-            }
-            float newX = (boundary[i].transform.position.x * boundarySize) + offsetX;
-            float newY = boundary[i].transform.position.y;
-            float newZ = (boundary[i].transform.position.z * boundarySize) + offsetZ;
-            boundary[i].transform.position = new Vector3(newX, newY, newZ);
-        }
-
-        for (int i = 4; i < boundary.Length; i++) 
-        {
-            float newX = boundary[i].transform.position.x * boundarySize;
-            float newY = boundary[i].transform.position.y * 1;
-            float newZ = boundary[i].transform.position.z * boundarySize;
-            boundary[i].transform.position =  new Vector3(newX, newY, newZ);
-        }
-
-        ground.transform.localScale = new Vector3 ( (float)boundarySize, 1, (float)boundarySize);
-
-        // now reback the AI walkable mesh area...
-        
+        AddNavMeshData();
     }
 
     void Update() {
         // maybe refresh if something makes the map change mid game...?
+    }
+    
+    void AddNavMeshData() {
+        // now rebake the AI Enemy walkable mesh area...
     }
 }
