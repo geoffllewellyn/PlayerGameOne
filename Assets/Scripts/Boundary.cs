30e39956c21d1be8b68edc9fdb675ac7501d3a04
geoffllewellyn <geoffllewellyn@gmail.com>
2020-08-15 03:05:26 -0700

    ground stuff

--- Assets/Scripts/Boundary.cs
+++ Assets/Scripts/Boundary.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boundary : MonoBehaviour {
-    GameObject[] boundary;
-    GameObject ground;
-
     public int boundarySize = 1;
 
+    GameObject[] boundary;
+    GameObject ground;
+    
     // Start is called before the first frame update
     void Start() {
         boundary = GameObject.FindGameObjectsWithTag ("Boundry");
         ground = GameObject.FindGameObjectsWithTag ("Ground")[0];
         
+        Vector3 setScale = new Vector3((float)boundarySize, 1, (float)boundarySize);
+        
+        print(boundary[0].name);
+        print(boundary[0].transform.localScale);
     }
 }
